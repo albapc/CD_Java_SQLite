14eca3dbc1258b4e3627ba8c8499b78842895ab5
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2019-05-21 11:17:55 +0200

    creación método insert

--- src/cd_proyecto_javasqlite/Consultas.java
+++ src/cd_proyecto_javasqlite/Consultas.java
@@ -1,6 +1,7 @@
 package cd_proyecto_javasqlite;
 
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
 
 public class Consultas {
@@ -43,5 +44,22 @@ public class Consultas {
             System.out.println("ERROR ---> " + ex);
         }
         return aux;
+    }
+    
+    public void insert(int referencia, String nombre, int nota, int id_pais) {
+        try {
+            //Declarar consulta
+            Conexion.s = Conexion.con.createStatement();
+            //Ejecutar consulta
+            Conexion.s.executeUpdate("INSERT INTO alumnos values (" + referencia + ",'" + nombre + "'," + nota + "," + id_pais + ")");
+            //Confirmacion
+//            System.out.println("Inserción realizada");
+        } catch (SQLIntegrityConstraintViolationException ex) {
+            System.out.println("ERROR CLAVE REPETIDA ---> " + ex);
+            //En caso de que repitamos la primary key volvera a lanzar el metodo
+            insert(Metodos.pedirReferencia(), Metodos.pedirNombre(), Metodos.pedirNota(), Metodos.pedirIdPais());
+        } catch (SQLException ex) {
+            System.out.println("ERROR ---> " + ex);
+        }
     }
 }
