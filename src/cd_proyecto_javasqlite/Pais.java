0b31c18054b463aaea05e1d4c260b10268d9e355
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2019-05-25 01:41:38 +0200

    crear clase Pais

---
+++ src/cd_proyecto_javasqlite/Pais.java
@@ -0,0 +1,37 @@
+package cd_proyecto_javasqlite;
+
+/**
+ *
+ * @author aperezcesar
+ */
+public class Pais {
+    
+    private int id_pais;
+    private String nombre_pais;
+    
+
+    public Pais() {
+    }
+
+    public Pais(int id_pais, String nombre_pais) {
+        this.id_pais = id_pais;
+        this.nombre_pais = nombre_pais;
+        
+    }
+
+    public String getNombre_pais() {
+        return nombre_pais;
+    }
+
+    public void setNombre_pais(String nombre_pais) {
+        this.nombre_pais = nombre_pais;
+    }
+
+    public int getId_pais() {
+        return id_pais;
+    }
+
+    public void setId_pais(int id_pais) {
+        this.id_pais = id_pais;
+    }
+}
